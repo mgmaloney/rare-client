cb6b546a8edc1cf18b39e791436cc3d78a3319ed
Matthew Maloney <matthewmaloneydev@gmail.com>
2023-11-08 20:13:27 -0600

    userContext set up

--- pages/login.js
+++ pages/login.js
@@ -4,13 +4,14 @@ import PropTypes from 'prop-types';
 import { useContext, useRef, useState } from 'react';
 import { loginUser } from '../utils/data/AuthManager';
 import UserContext from '../utils/context/userContext';
+import { getSingleUser } from '../utils/userData';
 
 function Login({ setToken }) {
   const username = useRef();
   const password = useRef();
   const navigate = useRouter();
   const [isUnsuccessful, setisUnsuccessful] = useState(false);
-  // const { setUser } = useContext(UserContext);
+  const { setUser } = useContext(UserContext);
 
   const handleLogin = async (e) => {
     e.preventDefault();
@@ -23,6 +24,8 @@ function Login({ setToken }) {
     const validLogin = JSON.parse(await loginUser(user));
     if (validLogin.valid === true) {
       setToken(validLogin.token);
+      const userLoggedIn = await getSingleUser(validLogin.token);
+      setUser(userLoggedIn);
       // set
       navigate.push('/');
     } else {
